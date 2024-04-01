b6e7b1d17454c83ef1c064d3db6ed5db22076241
Locustam Marinam <zoidberg0516@gmail.com>
2024-04-01 13:51:10 -0400

    Zoidberg's second fix
    Now the Boss image should be in, continuing work on other things

--- Assets/Scripts/ChatSystem/MessageData.cs
+++ Assets/Scripts/ChatSystem/MessageData.cs
@@ -4,5 +4,12 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "NewMessageData", menuName = "Message Data", order = 1)]
 public class MessageData : ScriptableObject
 {
-    public List<string> messageContentList = new List<string>();
+    public List<SingleMessage> messageContentList = new List<SingleMessage>();
+}
+
+[System.Serializable]
+public class SingleMessage
+{
+    public string message;
+    public Sprite sprite;
 }
