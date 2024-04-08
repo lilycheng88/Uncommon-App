e819a6a496352925caccb3f98c4fe7165a3b695c
Lucy Zhai <zz3635@nyu.edu>
2024-04-08 17:31:02 -0400

    Attempt to do login password

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+    public Selectable loginSelectable;
+    public Selectable passwordSelectable;
+    public string correctUsername = "1";
+    public string correctPassword = "1";
+    public string nextSceneName = "JiahaoTestScene"; // Name of the next scene
+
+    public void CheckCredentials()
+    {
+        string enteredUsername = GetInputFieldText(loginSelectable);
+        string enteredPassword = GetInputFieldText(passwordSelectable);
+
+        if (enteredUsername == correctUsername && enteredPassword == correctPassword)
+        {
+            // Credentials match, load the next scene
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.Log("Entered Username: " + enteredUsername);
+            Debug.Log("Entered Password: " + enteredPassword);
+            Debug.Log("Correct Username: " + correctUsername);
+            Debug.Log("Correct Password: " + correctPassword);
+            Debug.Log("Incorrect username or password!");
+        }
+    }
+
+    private string GetInputFieldText(Selectable selectable)
+    {
+        InputField inputField = selectable.GetComponentInChildren<InputField>();
+        if (inputField != null)
+        {
+            return inputField.text;
+        }
+        else
+        {
+            Debug.LogWarning("No InputField component found on selectable: " + selectable.name);
+            return "";
+        }
+    }
+
+    private void Update()
+    {
+        // Check if the user pressed Enter to attempt login
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            CheckCredentials();
+        }
+    }
+}
